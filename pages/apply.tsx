04be37dc7591ade1cba8344418de5e5177bb40cc
Dolphinbob <32620307+Dolphinbob@users.noreply.github.com>
2021-08-10 10:45:58 -0500

    Added some pages so they they at least exist

---
+++ pages/apply.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+import styles from '../styles/Home.module.css'
+import { InputGroup, Input, InputLeftAddon } from '@chakra-ui/input';
+import { LockIcon, AtSignIcon } from '@chakra-ui/icons';
+import { Heading, Link, Stack, Text } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/button';
+import { FormControl, FormLabel } from '@chakra-ui/react';
+
+export default function LoginPage() {
+    return (
+        <div className={styles.container}>
+          <Head>
+            <title>Selenium</title>
+            <meta name="description" content="Website for Selenium" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+    
+          <main className={styles.main}>
+            <Navbar />
+
+            <Heading>We are sorry, but we are not excepting applications at this time :(</Heading>
+          </main>
+        </div>
+    );
+}
---
+++ pages/login.tsx
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+import styles from '../styles/Home.module.css'
+import { InputGroup, Input, InputLeftAddon } from '@chakra-ui/input';
+import { LockIcon, AtSignIcon } from '@chakra-ui/icons';
+import { Heading, Link, Stack, Text } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/button';
+import { FormControl, FormLabel } from '@chakra-ui/react';
+
+export default function LoginPage() {
+    return (
+        <div className={styles.container}>
+          <Head>
+            <title>Selenium</title>
+            <meta name="description" content="Website for Selenium" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+    
+          <main className={styles.main}>
+            <Navbar />
+
+            <Heading>Login</Heading>
+
+            <Stack>
+                <InputGroup id="uname">
+                    <InputLeftAddon children={<AtSignIcon />}/>
+                    <Input placeholder="Username" />
+                </InputGroup>
+                <InputGroup id="pass">
+                    <InputLeftAddon children={<LockIcon />}/>
+                    <Input placeholder="Password" type="password" />
+                </InputGroup>
+                <Button onClick={login}>Login</Button>
+            </Stack>
+          </main>
+        </div>
+    );
+
+    function login() {
+        
+    }
+}
