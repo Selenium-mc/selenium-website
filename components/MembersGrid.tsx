b41ea452e1021f0fbd1882e2bc0b7f469f7deb5d
Alexander Park <parkalex380@gmail.com>
2021-08-10 21:47:46 -0500

    add styles, set display: flex to MemberCard

--- components/MembersGrid.tsx
+++ components/MembersGrid.tsx
@@ -1,11 +1,10 @@
-import { Box, Center, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import { Flex, Center, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import React from "react";
 
 
 export default function MemberGrid() {
     return (
-
         <SimpleGrid columns={5} spacing="10px">
             {/* That feeling when everyone is a banana */}
             <MemberCard name="Alex" photoUrl="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fweknowyourdreams.com%2Fimages%2Fbanana%2Fbanana-02.jpg&f=1&nofb=1" rank="Founder" />
@@ -19,9 +18,9 @@ export default function MemberGrid() {
             <MemberCard name="Oliver" photoUrl="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fweknowyourdreams.com%2Fimages%2Fbanana%2Fbanana-02.jpg&f=1&nofb=1" rank="Rookie" />
         </SimpleGrid>
     )
-}   
+}
 
-function MemberCard({name, photoUrl, rank}: any) {
+function MemberCard({ name, photoUrl, rank }: any) {
     let color: any;
     if (rank == "Rookie") {
         color = "rgb(31, 139, 76)";
@@ -32,10 +31,12 @@ function MemberCard({name, photoUrl, rank}: any) {
     }
 
     return (
-        <Box border="2px" borderColor={color} padding={4} borderRadius={10} w="120px">
-            <Image src={photoUrl} boxSize="80px" alt={name + "'s profile picture"}/>
-            <Text textAlign="center" fontWeight="bold">{name}</Text>
-            <Text textAlign="center">{rank}</Text>
-        </Box>
+        <div>
+            <Flex border="2px" borderColor={color} padding={4} borderRadius={10} w="120px">
+                <Image src={photoUrl} boxSize="80px" alt={name + "'s profile picture"} />
+                <Text textAlign="center" fontWeight="bold">{name}</Text>
+                <Text textAlign="center">{rank}</Text>
+            </Flex>
+        </div>
     );
 }
