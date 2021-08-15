48c5a51984012f3e0aef27a86299bf58504149bf
Alexander Park <parkalex380@gmail.com>
2021-08-15 12:38:43 -0500

    auth branch initial commit

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,37 +6,46 @@ import { LockIcon, AtSignIcon } from '@chakra-ui/icons';
 import { Heading, Link, Stack, Text } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/button';
 import { FormControl, FormLabel } from '@chakra-ui/react';
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 
 export default function LoginPage() {
-    return (
-        <div className={styles.container}>
-          <Head>
-            <title>Selenium</title>
-            <meta name="description" content="Website for Selenium" />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-    
-          <main className={styles.main}>
-            <Navbar />
+  const [session, loading] = useSession()
 
-            <Heading>Login</Heading>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Selenium</title>
+        <meta name="description" content="Website for Selenium" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
+      <main className={styles.main}>
+        <Navbar />
+
+        <Heading>Login</Heading>
+
+        {
+          !session ?
             <Stack>
-                <InputGroup id="uname">
-                    <InputLeftAddon><AtSignIcon /></InputLeftAddon>
-                    <Input placeholder="Username" />
-                </InputGroup>
-                <InputGroup id="pass">
-                    <InputLeftAddon><LockIcon /></InputLeftAddon>
-                    <Input placeholder="Password" type="password" />
-                </InputGroup>
-                <Button onClick={login}>Login</Button>
+              <InputGroup id="uname">
+                <InputLeftAddon><AtSignIcon /></InputLeftAddon>
+                <Input placeholder="Username" />
+              </InputGroup>
+              <InputGroup id="pass">
+                <InputLeftAddon><LockIcon /></InputLeftAddon>
+                <Input placeholder="Password" type="password" />
+              </InputGroup>
+              <Button onClick={login}>Login</Button>
             </Stack>
-          </main>
-        </div>
-    );
+            : <h1>You are logged in already you dummy</h1>
+        }
+      </main>
+    </div>
 
-    function login() {
-        
-    }
+  );
+
+  function login() {
+
+  }
 }
